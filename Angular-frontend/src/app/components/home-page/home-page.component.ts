73043906cd35c162ee6fcce0e9957de422f24b09
Rut Siman Tov <ruti8288@gmail.com>
2025-06-20 17:41:17 +0300

    Update home-page.component.ts

--- Angular-frontend/src/app/components/home-page/home-page.component.ts
+++ Angular-frontend/src/app/components/home-page/home-page.component.ts
@@ -13,7 +13,7 @@ import { Router } from '@angular/router';
 export class HomePageComponent {
   recipeArr: Recipe[] = []
   error: string = '';
-  imgUrl: string = process.env.baseUrl+"/images/"
+  imgUrl: string = process.env['baseUrl']+"/images/"
     // imgUrl: string = environment.baseUrl+"/images/"
 
 
