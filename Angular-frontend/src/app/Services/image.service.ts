4e371a632790bd0451768e2aaee4babf0e57fff4
Rut Siman Tov <ruti8288@gmail.com>
2025-06-20 17:17:50 +0300

    Update api url in image.service.ts

--- Angular-frontend/src/app/Services/image.service.ts
+++ Angular-frontend/src/app/Services/image.service.ts
@@ -12,6 +12,9 @@ export class ImageService {
   onUpload(image: File): Observable<any> {
     const fromData = new FormData()
     fromData.append('file', image, image.name);
-    return this.httpC.post(environment.baseUrl+'/image/upload', fromData)
+    return this.httpC.post(process.env.baseUrl+'/image/upload', fromData)
+        // return this.httpC.post(environment.baseUrl+'/image/upload', fromData)
+
+
   }
 }
