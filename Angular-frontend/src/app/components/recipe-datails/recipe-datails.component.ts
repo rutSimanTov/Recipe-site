a39cc1bd3cd487a8cfe1cafb673777a2ccb0e69b
Rut Siman Tov <ruti8288@gmail.com>
2025-06-20 17:19:30 +0300

    Update recipe-datails.component.ts

--- Angular-frontend/src/app/components/recipe-datails/recipe-datails.component.ts
+++ Angular-frontend/src/app/components/recipe-datails/recipe-datails.component.ts
@@ -26,7 +26,9 @@ export class RecipeDatailsComponent implements OnInit {
     duration: 0, servingSize: 0, instructions: "", userId: 0
   }
 
-  imgUrl: string = environment.baseUrl+"/images/"
+  imgUrl: string =  process.env.baseUrl+"/images/"
+    // imgUrl: string = environment.baseUrl+"/images/"
+
 
 
   ingredients: RecipeIngredientDTO[] = []
