ec085e751dc70e6156d1da71bb35d4ba2496b717
Rut Siman Tov <ruti8288@gmail.com>
2026-05-26 17:59:29 +0300

    Update server routes for recipe details and prerendering

--- Angular-frontend/src/app/app.routes.server.ts
+++ Angular-frontend/src/app/app.routes.server.ts
@@ -1,6 +1,20 @@
+// import { RenderMode, ServerRoute } from '@angular/ssr';
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender
+//   }
+// ];
+
+
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'recipeDetailes/:id', 
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
